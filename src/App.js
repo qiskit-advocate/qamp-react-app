a0d019ed14ce2d36db773604d2391a7f7b4e6af0
Abby-Mitchell <abby.s.mitchell@gmail.com>
2021-10-06 18:33:51 +0100

    Add main app content and styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { useEffect, useState } from 'react';
+import { Octokit } from 'octokit';
+
+import { Header, HeaderName,} from "carbon-components-react/lib/components/UIShell";
+import { Accordion, AccordionItem, Tabs, Tab } from 'carbon-components-react'
+import ReactMarkdown from 'react-markdown'
+
+const octokit = new Octokit();
 
 function App() {
+  const [ S21issues, setS21Issues ] = useState([]);
+  const [ F21issues, setF21Issues ] = useState([]);
+
+  useEffect(() => {
+    async function getIssues(repoName) {
+      const response = await octokit.request(`GET /repos/qiskit-advocate/${repoName}/issues`, {
+        per_page: 100,
+        sort: 'created',
+        direction: 'asc'
+      })
+      return response
+    }
+
+    // get all issues from qamp-fall-21 repo
+    getIssues('qamp-fall-21').then(res => {
+      // filter out pull requests
+      const justIssues = res.data.filter(issue => !issue.pull_request)
+      console.log(justIssues)
+      setF21Issues(justIssues)
+    })
+
+    // get all issues from qamp-spring-21 repo
+    getIssues('qamp-spring-21').then(res => {
+      // filter out pull requests
+      const justIssues = res.data.filter(issue => !issue.pull_request)
+      console.log(justIssues)
+      setS21Issues(justIssues)
+    })
+    
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header aria-label="IBM Platform Name">
+        <HeaderName href="#" prefix="IBM">
+          Qiskit Advocate Mentorship Program (QAMP)
+        </HeaderName>
+      </Header>
+      <div style={{margin: "10px", marginTop: "50px"}}>
+        <Tabs>
+          <Tab id="tab-1" label="Fall 2021">
+            <Accordion>
+              {F21issues.map(issue => {
+                return <AccordionItem title={`#${issue.number} ${issue.title}`}>
+                  <h3>{issue.title}</h3>
+                  <ReactMarkdown className="markDownElem">{issue.body}</ReactMarkdown>
+                </AccordionItem>
+              })}
+            </Accordion>
+          </Tab>
+          <Tab id="tab-2" label="Spring 2021">
+            <Accordion>
+              {S21issues.map(issue => {
+                return <AccordionItem title={`#${issue.number} ${issue.title}`}>
+                  <h3>{issue.title}</h3>
+                  <ReactMarkdown className="markDownElem">{issue.body}</ReactMarkdown>
+                </AccordionItem>
+              })}
+            </Accordion>
+          </Tab>
+        </Tabs>
+      </div>
+      
     </div>
   );
 }
